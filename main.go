5bcd73ed59296f8eeafb9ee279f8a4ff0725e279
Logan Krause <logankrause16@gmail.com>
2020-03-18 15:47:32 -0400

    End of day commit

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func dataLink(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome home!")
+}
 
 func main() {
-	fmt.Printf("Hello World! \n")
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/", dataLink)
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
